ec0690014f83e66a9362d2f82252b1fea2b1295f
razz.ernesto@gmail.com <ernesto@muuvment.com>
2025-12-19 10:47:19 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button variant="default">Click me</Button>
+<Button variant="outline">Click me</Button>
+<Button variant="secondary">Click me</Button>
+<Button variant="ghost">Click me</Button>
+<Button variant="link">Click me</Button>
