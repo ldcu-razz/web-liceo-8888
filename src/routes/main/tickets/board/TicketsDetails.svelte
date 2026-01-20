e239504784056e3764f1a524b34a377f1984309d
Christian Carl C. Cabalde <cccabalde30858@liceo.edu.ph>
2026-01-20 21:51:03 +0800

    fix(admin/user): super admin history loading has scrollable select dropdown which made it flicker

--- src/routes/main/tickets/board/TicketsDetails.svelte
+++ src/routes/main/tickets/board/TicketsDetails.svelte
@@ -340,7 +340,7 @@
 				{/if}
 
 				{#if activeTab === historyTab}
-					<div class="max-h-[calc(80vh-230px)] overflow-y-auto">
+					<div class="max-h-[calc(80vh-230px)]">
 						<TicketHistory ticketId={ticket.id} isAnonymous={isAnonymous} />
 					</div>
 				{/if}
